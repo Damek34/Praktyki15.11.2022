cbe7abc3c0133a1a1bbe325de3e00aa82a5ec486
Damian Cichosz <damekjodamek@gmail.com>
2022-11-12 01:04:14 +0100

    zad2

--- script.js
+++ script.js
@@ -31,11 +31,41 @@ function podziel()
 
     if(liczba2!=0)
     {
-        document.getElementById("wynik").innerHTML = liczba1 + liczba2;
+        document.getElementById("wynik").innerHTML = liczba1 / liczba2;
     }
     else
     document.getElementById("wynik").innerHTML = "Nie można dzielić przez 0!";
+}
 
+
+function palindrome()
+{
+
+    var slowo = document.getElementById("slowo").value;
+    var palindrom = [];
+    var palindrom_string = "";
+    var j = 0;
+
+    for(var i = slowo.length-1; i >= 0; i--, j++)
+    {
+        palindrom[j] = slowo[i];
+    }
+
+    for(var i = 0; i < slowo.length; i++)
+    {
+        palindrom_string += palindrom[i];
+    }
+    
+    if(slowo == palindrom_string)
+    {
+        document.getElementById("wynik2").innerHTML = "Słowo jest palindromem";
+    }
+    else
+    {
+        document.getElementById("wynik2").innerHTML = "Słowo nie jest palindromem";
+    }
 
     
+
+
 }
